532fb82ec75b62082d5b7cad8d349c151262f046
omkar2205 <coolomkar11@gmail.com>
2026-06-07 19:36:14 +0530

    Repair frontend public brand text

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 /* =========================================================
-  Solvanta Learning
+  Solvanta Consulting
   Site Configuration
   ========================================================= */
 
@@ -7,7 +7,7 @@ window.SOLVANTA_CONFIG = {
 
   API_URL: "https://script.google.com/macros/s/AKfycbx0NQWTGVWjR668hpGyYDcQtEJHcE9exFOm3V-R1SWXWUBQ6e5MOTj6nGc665ppH7v7og/exec",
 
-  SITE_NAME: "Solvanta Learning",
+  SITE_NAME: "Solvanta Consulting",
 
   SUPPORT_EMAIL: "helpdesk@solvantaconsulting.com"
 
